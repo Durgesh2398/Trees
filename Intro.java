3b58d7c366ad65b86202a0a46002161b1fb0c085
Durgesh2398 <2308durgesh@gmail.com>
2020-03-27 12:56:52 +0530

    height and level data

--- Intro.java
+++ Intro.java
@@ -44,6 +44,38 @@ public class Intro {
 		}
 		return leafNodes(root.left)+leafNodes(root.right);
 	}
+	
+	//to calcuate sum of leaf nodes
+	int sumLeafNodes(Node root) {
+		if(root==null) {
+			return 0;
+		}
+		if(root.left==null&&root.right==null) {
+			return root.data;
+		}
+		return sumLeafNodes(root.left)+sumLeafNodes(root.right);
+	}
+	
+	//to get hight of tree
+	int height(Node root) {
+		if(root==null) {
+			return -1;
+		}
+		return 1+Math.max(height(root.left),height(root.right)); 
+	}
+	
+	//
+	void printAtLevel(Node root,int level) {
+		if(root==null) {
+			return ;
+		}
+		if(level==1) {
+			System.out.print(root.data+" ");
+			return ;
+		}
+		printAtLevel(root.left,level-1);
+		printAtLevel(root.right,level-1);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Intro ob= new Intro(2);//binary tree with root node 2
@@ -55,6 +87,20 @@ public class Intro {
 		System.out.println(ob.TreeSum(ob.root));
 		System.out.println(ob.countNodes(ob.root));
 		System.out.println(ob.leafNodes(ob.root));
+		System.out.println(ob.sumLeafNodes(ob.root));
+		System.out.println(ob.height(ob.root));
+		System.out.print("Nodes at lvel 1  :");
+		ob.printAtLevel(ob.root, 1);
+		System.out.println("");
+		System.out.print("Nodes at lvel 2  :");
+		ob.printAtLevel(ob.root, 2);
+		System.out.println("");
+		System.out.print("Nodes at lvel 3  :");
+		ob.printAtLevel(ob.root, 3);
+		System.out.println("");
+		System.out.print("Nodes at lvel 4  :");
+		ob.printAtLevel(ob.root, 4);
+		System.out.println("");
 	}
 
 }
